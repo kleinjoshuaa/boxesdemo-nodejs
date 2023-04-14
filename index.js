34702c076c4793850700a6a21d1146071ce2bbfb
Joshua Klein <klein.joshua.a@gmail.com>
2023-04-14 15:26:46 -0400

    updated to use client side sdk

--- index.js
+++ index.js
@@ -8,114 +8,9 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const db = trivialdb.db('sessionVars');
 
-let SplitFactory = require('@splitsoftware/splitio').SplitFactory;
+//let SplitFactory = require('@splitsoftware/splitio').SplitFactory;
 const { create } = require("domain");
 
-
-function createTableData() {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
-    const account = [
-    ["Nike", "Nike", "Nike", "Apple", "Apple", "Apple", "LinkedIn", "Best Buy", "Best Buy", "Best Buy"],
-    ["Nike", "Nike", "Nike", "Apple", "Apple", "Apple", "LinkedIn", "Best Buy", "Best Buy", "Best Buy"],
-    ["Nike", "Nike", "Nike", "Apple", "Apple", "Apple", "LinkedIn", "Best Buy", "Best Buy", "Best Buy"],
-    ["Google", "Google", "Google", "Microsoft", "Microsoft", "Microsoft", "Microsoft", "Best Buy", "Best Buy", "Best Buy"],
-    ["Google", "Google", "Google", "Microsoft", "Microsoft", "Microsoft", "Microsoft", "Pintrest", "Pintrest", "Pintrest"],
-    ["Google", "Google", "Google", "Microsoft", "Microsoft", "Microsoft", "Microsoft", "Pintrest", "Pintrest", "Pintrest"],
-    ["Dell", "Dell", "Dell", "Microsoft", "Microsoft", "Microsoft", "Microsoft", "Pintrest", "Pintrest", "Pintrest"],
-    ["Zoom", "Zoom", "Zoom", "Slack", "Slack", "Slack", "Samsung", "Disney", "Disney", "Disney"],
-    ["Zoom", "Zoom", "Zoom", "Slack", "Slack", "Slack", "Samsung", "Disney", "Disney", "Disney"],
-    ["Zoom", "Zoom", "Zoom", "Slack", "Slack", "Slack", "Samsung", "Disney", "Disney", "Disney"]
-]; // theoretically you could expand this table if you wanted. It would be a non-trivial exercise to get realistic clusters like this though
-    let table = [];
-    for (let i = 0; i < 10; i += 1) {
-        table.push([]);
-        for (let j = 0; j < 10; j += 1 ) {
-            table[i][j] = {user: alphabet[i]+j, account: account[i][j]}
-        }
-    }
-    return table;
-}
-
-const table = createTableData();
-
-function buildTable(table, splitClient, splitName, eventName) {
-    let htmlString = '';
-    for(let rowIdx = 0; rowIdx < table.length; rowIdx += 1) {
-        htmlString += "<tr>";
-        let row = table[rowIdx];
-        for(let colIdx = 0; colIdx < row.length; colIdx += 1) {
-            let rowElem = row[colIdx];
-            let attributes = {
-				row: rowElem.user.charAt(0),
-				col: rowElem.user.charAt(1),
-				userid: rowElem.user,
-				account: rowElem.account
-			}
-            let splitResult = splitClient.getTreatmentWithConfig(rowElem.user, splitName, attributes);
-            let configs = {
-                            popup_value: '',
-                            popup_message: 'coming soon',
-                            font_size: 'medium'
-                        }; // set default configs
-            
-            if(splitResult.config) {
-                configs = JSON.parse(splitResult.config); //overide with configs from treatment
-            }
-            let treatmentColor;
-            switch(splitResult.treatment) {
-                case "standard":
-                    treatmentColor = "rgb(255, 70, 82);";
-                    break;
-                case "premium":
-                    treatmentColor = "rgb(0, 124, 156);";
-                    break;
-                case "current":
-                    treatmentColor = "rgb(173, 193, 116);";
-                    break;
-                default:
-                    treatmentColor = "red;";
-                    break;
-            }
-            htmlString += `<td style="font-size:${configs.font_size}; color:white; width:40px; height:40px; text-align:center; background-color:${treatmentColor}" `;
-            htmlString += `onclick= "createPopup({user:'${rowElem.user}', message:'${configs.popup_message}', eventName:'${eventName}', treatment:'${splitResult.treatment}', value:'${configs.popup_value}' })">${rowElem.user}</td>`;
-        }
-        htmlString += "</tr>";    
-    }
-    
-    
-
-    return htmlString
-}
-
-
-function createFactoryArgs(debug) {
-return {
-    startup: {
-        requestTimeoutBeforeReady: 0.8, // 800ms
-        retriesOnFailureBeforeReady: 1, // 1 sec
-        readyTimeout: 1.5, // 1.5 sec
-        eventsFirstPushWindow: 10 // 10 sec
-    },
-    core: {
-        authorizationKey: enVars.API_KEY,
-        trafficType: 'user',
-        labelsEnabled: true
-    },
-    scheduler: {
-        featuresRefreshRate: 1, // 30 sec 
-        segmentsRefreshRate: 60, // 60 sec 
-        metricsRefreshRate: 60, // 60 sec
-        impressionsRefreshRate: 60, // 60 sec
-        eventsPushRate: 60, // 60 sec
-        eventsQueueSize: 500 // 500 items
-    },
-    streamingEnabled: true,
-    debug: debug
-    }
-}
-
-// not checking for rediness here, instead we will check when we call getTreatment
-const splitClient = SplitFactory(createFactoryArgs(false)).client();
 
 app.set('view engine', 'ejs');
 
@@ -130,64 +25,7 @@ app.get("/", (req, res) => {
 });
 
 
-    app.get("/boxStream", (req, res) => {
-      res.setHeader("Cache-Control", "no-store");
-      res.setHeader("Content-Type", "text/event-stream");
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Connection", "keep-alive");
-      res.flushHeaders(); // flush the headers to establish SSE with client
-      let cookie = req.cookies.SPLIT_BOX_DEMO; // get the split name and the event name from the cookie reference
-
-      db.load(cookie, "default").then(function (val) {
-        if (val == "default") {
-          console.error("failed reading cookie for updating the boxes");
-        }
-        // send heartbeat   
-        const interval = 60000; // every minute
-        let intervalId = setInterval(function() {
-          res.write(`:\n\n`);
-        }, interval);
-
-
-        // send if ready 
-        // (since the SDK client is created upon server startup we expect this to be ready rather than polling for the SDK_READY event which likely already was fired by the time we get here)
-        splitClient.ready().then(() => {
-          res.write(
-            `data: ${buildTable(
-              table,
-              splitClient,
-              val.splitName,
-              val.eventName
-            )}\n\n`
-          );
-
-
-        // update upon SDK_UPDATE, no polling or refresh needed!
-        splitClient.on(splitClient.Event.SDK_UPDATE, () => {
-          res.write(
-            `data: ${buildTable(
-              table,
-              splitClient,
-              val.splitName,
-              val.eventName
-            )}\n\n`
-          );
-        });
-        res.write()
-        // If client closes connection, stop sending events
-        res.on("close", () => {
-          console.log("client dropped");
-          res.end();
-        });
-
-
-
-        });
-
-
-      });
-    });
-
+    
 
 app.post("/login", (req, res) => {
   const { eventName, splitName } = req.body;
@@ -197,24 +35,10 @@ app.post("/login", (req, res) => {
   }).then(function (cookieId) {
     res.cookie("SPLIT_BOX_DEMO", cookieId, { maxAge: 900000, httpOnly: true });
     console.log("cookie created successfully");
-    res.render("demo",{} );
+    res.render("demo",{splitName: splitName, event: eventName, apiKey: enVars.API_KEY} );
   });
 });
 
-  app.post("/track", function (req, res) {
-    const { user, eventName, value, treatment } = req.body;
-    let trackResult = splitClient.track(
-      user,
-      "user",
-      eventName,
-      parseFloat(value),
-      { treatment_string: treatment }
-    );
-    trackResult
-      ? console.log("tracked successfully on the server")
-      : console.log("tracking failed on the server");
-    res.sendStatus(200);
-  });
 
 
 app.listen(3000, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,44 +1,47 @@
 
-function postEvent(data) {
-    fetch("/track", {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: `user=${data.user}&eventName=${data.eventName}&value=${data.value}&treatment=${data.treatment}`,
-    }).then( (data) => {
-      console.log('Event tracked!')
-    })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+// function postEvent(data) {
+//     fetch("/track", {
+//       method: "POST", // or 'PUT'
+//       headers: {
+//         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+//       },
+//       body: `user=${data.user}&eventName=${data.eventName}&value=${data.value}&treatment=${data.treatment}`,
+//     }).then( (data) => {
+//       console.log('Event tracked!')
+//     })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//       });
+//   }
   
-  function createPopup(parms) {
-      let message = parms.message;
-      let eventName = parms.eventName;
-      let treatment = parms.treatment;
-      let value = parms.value;
+//   function createPopup(parms) {
+//       let message = parms.message;
+//       let eventName = parms.eventName;
+//       let treatment = parms.treatment;
+//       let value = parms.value;
   
-      if (message != null) {
-        alert(message + " " + value);
-      }
-      if (value != null && eventName != "") {
-        analytics.identify(parms.user, {
-          row: parms.user.substr(0,1),
-          col: parms.user.substr(1)
-        });
-        analytics.track('clicked_from_segment', {parms});
-        result = postEvent(parms);
-        }
+//       if (message != null) {
+//         alert(message + " " + value);
+//       }
+//       if (value != null && eventName != "") {
+//         analytics.identify(parms.user, {
+//           row: parms.user.substr(0,1),
+//           col: parms.user.substr(1)
+//         });
+//         analytics.track('clicked_from_segment', {parms});
+//         result = postEvent(parms);
+//         }
            
-    };
+//     };
   
-    const source = new EventSource('/boxStream');
+//     const source = new EventSource('/boxStream');
 
-      source.addEventListener('message', message => {
-        console.log('Got Message');
-        // Display the event data in the myTable table
-          document.querySelector('#myTable').innerHTML = message.data;
-      });
+//       source.addEventListener('message', message => {
+//         console.log('Got Message');
+//         // Display the event data in the myTable table
+//           document.querySelector('#myTable').innerHTML = message.data;
+//       });
 
+//       setTimeout(function () {
+//         window.location.reload();
+//     }, 2000);
